4dbd38256941f87b2c119fdcb0153bd21684d24c
Vũ Thế Huy <31652864+vuthehuyht@users.noreply.github.com>
2019-03-30 20:40:43 +0700

    fix format ghi file chat.log

--- Server/Server/Logger.cpp
+++ Server/Server/Logger.cpp
@@ -11,9 +11,12 @@ Logger::~Logger()
 {
 }
 
-void Logger::WriteMessagse(std::string message) {
-	f.open("chat.log", std::ios::in);
+void Logger::WriteMessagse(std::string message, std::string username) {
+	char str[32]{};
+	t = time(0);
+	struct tm* time_info = localtime(&t);
+	f.open("chat.log", std::ios::out);
 	if (f.fail())
 		std::cerr << "Opening failed" << std::endl;
-	else f << message.c_str() << std::endl;
+	else f<< time_info->tm_year + 1900 << "-" << time_info->tm_mon + 1 << "-" <<time_info->tm_mday << " " << time_info->tm_hour<< ":" <<time_info->tm_min <<":" << time_info->tm_sec+ 1 << " " << username << " " << message << std::endl;
 }
